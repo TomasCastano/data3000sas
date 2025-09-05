a0fceb563cdbfc3c71bf163511b415b3c2d54eaf
TomasCastano <tomascastanochica@gmail.com>
2025-09-05 14:53:25 -0500

    Add services dynamic data

--- src/pages/Servicios/ServiceCard/ServiceCard.jsx
+++ src/pages/Servicios/ServiceCard/ServiceCard.jsx
@@ -1,28 +1,31 @@
-import image from '../../../assets/images/almacenamiento2.webp'
+import image from '../../../assets/images/almacenamiento4.webp'
 import QueryStatsRoundedIcon from '@mui/icons-material/QueryStatsRounded'
 import TaskAltRoundedIcon from '@mui/icons-material/TaskAltRounded'
 import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded'
 
 import './ServiceCard.css'
 
-const ServiceCard = () => {
+const ServiceCard = ({ id, service }) => {
+
+    console.log(service)
+
     return (
-        <div className="service-card">
+        <div id={id} className="service-card">
             <div className="service-info">
                 <div className="service-card-header">
                     <div className="icon-container">
                         <QueryStatsRoundedIcon />
                     </div>
-                    <h3>Diagnóstico</h3>
+                    <h3>{service.title}</h3>
                 </div>
                 <div className="service-card-body">
-                    <p>Un análisis consciente permite conservar información relevante a pesar del deterioro del material en que está contenida. Una vez concluye la revisión, se procede al diseño de un informe en el que se evidencia el estado del material recibido y de las acciones que deben llevarse a cabo para su preservación.</p>
+                    <p>{service.description}</p>
                     <ul>
-                        <li><TaskAltRoundedIcon className='icon' /> Evaluación completa del estado documental</li>
-                        <li><TaskAltRoundedIcon className='icon' /> Detección de riesgos y vulnerabilidades</li>
-                        <li><TaskAltRoundedIcon className='icon' /> Análisis de material deteriorado</li>
-                        <li><TaskAltRoundedIcon className='icon' /> Propuestas de soluciones personalizadas</li>
-                        <li><TaskAltRoundedIcon className='icon' /> Informes detallados de diagnóstico</li>
+                        <li><TaskAltRoundedIcon className='icon' /> {service.list[0]}</li>
+                        <li><TaskAltRoundedIcon className='icon' /> {service.list[1]}</li>
+                        <li><TaskAltRoundedIcon className='icon' /> {service.list[2]}</li>
+                        <li><TaskAltRoundedIcon className='icon' /> {service.list[3]}</li>
+                        <li><TaskAltRoundedIcon className='icon' /> {service.list[4]}</li>
                     </ul>
                     <button className="service-card-btn">Cotizar Servicio <KeyboardArrowRightRoundedIcon className="icon" /></button>
                 </div>
