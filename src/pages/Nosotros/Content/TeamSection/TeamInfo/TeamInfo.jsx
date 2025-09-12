cb7c79dc4daee2946ac668b4456a5a8e0f0ecb01
TomasCastano <tomascastanochica@gmail.com>
2025-09-12 10:45:56 -0500

    Modify team card style

--- src/pages/Nosotros/Content/TeamSection/TeamInfo/TeamInfo.jsx
+++ src/pages/Nosotros/Content/TeamSection/TeamInfo/TeamInfo.jsx
@@ -1,5 +1,7 @@
 import servicesImage from '../../../../../assets/images/services.webp'
 import LaptopChromebookRoundedIcon from '@mui/icons-material/LaptopChromebookRounded'
+import DocumentScannerOutlinedIcon from '@mui/icons-material/DocumentScannerOutlined'
+import Diversity2OutlinedIcon from '@mui/icons-material/Diversity2Outlined'
 
 import './TeamInfo.css'
 
@@ -7,15 +9,18 @@ const TeamInfo = () => {
     const teamData = [
         {
             title: "Tecnologías de la Información",
-            description: "Soluciones tecnológicas avanzadas para la gestión digital de documentos y sistemas de información."
+            description: "Soluciones tecnológicas avanzadas para la gestión digital de documentos y sistemas de información.",
+            icon: LaptopChromebookRoundedIcon
         },
         {
             title: "Gestión Documental",
-            description: "Contamos con el personal capacitado para el desempeño de cada una de las actividades involucradas en el proceso de la gestión documental."
+            description: "Contamos con el personal capacitado para el desempeño de cada una de las actividades involucradas en el proceso de la gestión documental.",
+            icon: DocumentScannerOutlinedIcon
         },
         {
             title: "Talento Humano",
-            description: "El área de talento humano, nunca te hará sentir lejos de casa, acompañándote en toda dificultad, capacitándote y enseñándote cada vez más."
+            description: "El área de talento humano, nunca te hará sentir lejos de casa, acompañándote en toda dificultad, capacitándote y enseñándote cada vez más.",
+            icon: Diversity2OutlinedIcon
         }
     ]
 
@@ -28,8 +33,8 @@ const TeamInfo = () => {
                     </div>
                     <div class="team-card-info">
                         <div class="team-info-header">
-                            <LaptopChromebookRoundedIcon className='icon-bg' />
-                            <LaptopChromebookRoundedIcon className='icon' />
+                            <item.icon className='icon-bg' />
+                            <item.icon className='icon' />
                             <h3>{item.title}</h3>
                         </div>
                         <p>{item.description}</p>
