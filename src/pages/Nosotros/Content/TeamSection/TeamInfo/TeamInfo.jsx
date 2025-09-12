62ec1c635a1cae53793fde0ba38ab1941937dc05
TomasCastano <tomascastanochica@gmail.com>
2025-09-12 11:38:14 -0500

    Update team images and enhance texts

--- src/pages/Nosotros/Content/TeamSection/TeamInfo/TeamInfo.jsx
+++ src/pages/Nosotros/Content/TeamSection/TeamInfo/TeamInfo.jsx
@@ -1,6 +1,9 @@
+import tiImage from '../../../../../assets/images/ti.webp'
+import thImage from '../../../../../assets/images/th.webp'
 import servicesImage from '../../../../../assets/images/services.webp'
+
 import LaptopChromebookRoundedIcon from '@mui/icons-material/LaptopChromebookRounded'
-import DocumentScannerOutlinedIcon from '@mui/icons-material/DocumentScannerOutlined'
+import TopicOutlinedIcon from '@mui/icons-material/TopicOutlined'
 import Diversity2OutlinedIcon from '@mui/icons-material/Diversity2Outlined'
 
 import './TeamInfo.css'
@@ -9,18 +12,21 @@ const TeamInfo = () => {
     const teamData = [
         {
             title: "Tecnologías de la Información",
-            description: "Soluciones tecnológicas avanzadas para la gestión digital de documentos y sistemas de información.",
-            icon: LaptopChromebookRoundedIcon
+            description: "Desarrollamos soluciones tecnológicas que fortalecen la seguridad de la información y optimizan la gestión documental, mejorando la eficiencia y precisión en los datos.",
+            icon: LaptopChromebookRoundedIcon,
+            image: tiImage
         },
         {
             title: "Gestión Documental",
-            description: "Contamos con el personal capacitado para el desempeño de cada una de las actividades involucradas en el proceso de la gestión documental.",
-            icon: DocumentScannerOutlinedIcon
+            description: "Contamos con un equipo especializado que garantiza orden, control y eficiencia en cada etapa del proceso de gestión documental, asegurando calidad y confiabilidad.",
+            icon: TopicOutlinedIcon,
+            image: servicesImage
         },
         {
             title: "Talento Humano",
-            description: "El área de talento humano, nunca te hará sentir lejos de casa, acompañándote en toda dificultad, capacitándote y enseñándote cada vez más.",
-            icon: Diversity2OutlinedIcon
+            description: "Nuestro equipo de Talento Humano impulsa el crecimiento de cada colaborador con capacitación, apoyo constante y un ambiente laboral cercano.",
+            icon: Diversity2OutlinedIcon,
+            image: thImage
         }
     ]
 
@@ -29,7 +35,7 @@ const TeamInfo = () => {
             {teamData.map((item, index) => (
                 <div class="team-card" key={index}>
                     <div class="team-card-image">
-                        <img src={servicesImage.src} alt="Personal de DATA 3000 S.A.S" />
+                        <img src={item.image.src} alt="Personal de DATA 3000 S.A.S" />
                     </div>
                     <div class="team-card-info">
                         <div class="team-info-header">
