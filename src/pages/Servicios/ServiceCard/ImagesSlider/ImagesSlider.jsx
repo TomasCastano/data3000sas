1b7f7dcd86c334b97ab42870a35cf92bb6370d3b
TomasCastano <tomascastanochica@gmail.com>
2025-09-24 09:15:54 -0500

    Redesign the services card slider responsive

--- src/pages/Servicios/ServiceCard/ImagesSlider/ImagesSlider.jsx
+++ src/pages/Servicios/ServiceCard/ImagesSlider/ImagesSlider.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState, useMemo } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay } from 'swiper/modules'
 
@@ -9,6 +10,22 @@ import { Pagination } from 'swiper/modules'
 import './ImagesSlider.css'
 
 const ImagesSlider = ({ images }) => {
+
+    const [windowWidth, setWindowWidth] = useState(0);
+
+    useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth)
+
+        setWindowWidth(window.innerWidth)
+        window.addEventListener('resize', handleResize)
+        
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+    const slidesPerView = useMemo(() => {
+        return windowWidth > 1050 ? 1 : windowWidth > 615 ? 3 : 2
+    }, [windowWidth])
+
     return (
         <Swiper
             className="service-swiper"
@@ -17,6 +34,8 @@ const ImagesSlider = ({ images }) => {
             autoplay={{ delay: 2500 }}
             loop={true}
             bulletclass="swiper-pagination"
+            spaceBetween={15}
+            slidesPerView={slidesPerView}
         >
             {images.map((image, index) => (
                 <SwiperSlide key={index}>
